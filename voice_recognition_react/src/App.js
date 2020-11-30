eaa43587cdbad52443e2220117a9a6cab0dfcba6
TahzidMahmud <tahzid15-1272@diu.edu.bd>
2020-11-30 13:24:56 +0600

    all alan commands are integrated now highlighting & scrolling while reading is left

--- voice_recognition_react/src/App.js
+++ voice_recognition_react/src/App.js
@@ -6,30 +6,30 @@ import useStyles from "./styles.js";
 const App = () => {
   const [newArticles, setnewArticles] = useState([]);
   useEffect(() => {
-    // alanBtn({
-    //   key:
-    //     "7c216fe9ef86871f1be9cfbe0b5e25042e956eca572e1d8b807a3e2338fdd0dc/stage",
-    //   onCommand: (commandData) => {
-    //     if (commandData.command === "new headlines") {
-    //       console.log(commandData.saved_articles);
-    //       let articles = commandData.saved_articles;
-    //       setnewArticles(articles);
-    //     }
-    //   },
-    // });
+    alanBtn({
+      key:
+        "7c216fe9ef86871f1be9cfbe0b5e25042e956eca572e1d8b807a3e2338fdd0dc/stage",
+      onCommand: (commandData) => {
+        if (commandData.command === "new headlines") {
+          console.log(commandData.saved_articles);
+          let articles = commandData.saved_articles;
+          setnewArticles(articles);
+        }
+      },
+    });
     // place holder api code start
-    let url =
-      "https://newsapi.org/v2/top-headlines?sources=bbc-news&apiKey=6fb015a987474e7d9eb5ee7ff92f90c1";
-    axios
-      .get(url)
-      .then((res) => {
-        console.log(res.data.articles);
-        let articles = res.data.articles;
-        // setnewArticles(articles);
-      })
-      .catch((err) => {
-        console.log(err);
-      });
+    // let url =
+    //   "https://newsapi.org/v2/top-headlines?sources=bbc-news&apiKey=6fb015a987474e7d9eb5ee7ff92f90c1";
+    // axios
+    //   .get(url)
+    //   .then((res) => {
+    //     console.log(res.data.articles);
+    //     let articles = res.data.articles;
+    //     // setnewArticles(articles);
+    //   })
+    //   .catch((err) => {
+    //     console.log(err);
+    //   });
     //place holder api cdoe end
   }, []);
   const classes = useStyles();
